e59477d34ba0af1ef9cfa9e768bf95dd1653048f
Levi <108506153+levithebevi@users.noreply.github.com>
2022-11-17 23:37:49 -0500

    Adds --path argument

--- mount2.c
+++ mount2.c
@@ -2,17 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main( int argc, char** argv )
-
-{
+int main( int argc, char** argv ) {
 
 	char answer;
 	char drive[30];
 	char mount[30] = "sudo mount ";
-	char mountpoint[15] = " /mountpoint";
+	char mountpoint[30] = " /mountpoint";
+	
+if( argv = "--path" )
+{
+	printf( "Type the path you would like to mount the drive in. (Putting a space after the : is required.):");
+	gets( mountpoint );
 
 	system( "lsblk" );
-	printf( "What drive would you like to mount?: " );
+	printf( "\nWhat drive would you like to mount?: " );
+	gets( drive );
+	strcat( mount,drive );
+	strcat( mount,mountpoint );
+
+	printf( "\nYou typed: " );
+	puts( drive );
+
+	printf( "\nAre you sure you want to mount this? (Y/N) " );
+	
+	scanf( " %c", &answer );
+	printf( "\nSelected %c\n", answer );
+	
+	if ( answer == 'Y' )
+		
+	{
+
+		system( mount );
+
+	return 0;
+
+	}
+	
+	else if ( answer == 'y' )
+	
+	{	
+
+		system( mount );
+		
+	return 0;
+	
+	}
+		else
+	{
+		printf( "Aborted!" );
+		return 0;
+	}
+}
+
+else
+
+{
+
+printf( "Warning: Path not specified, mounting in /mountpoint by default." );
+
+	system( "lsblk" );
+	printf( "\nWhat drive would you like to mount?: " );
 	gets( drive );
 	strcat( mount,drive );
 	strcat( mount,mountpoint );
@@ -43,11 +92,13 @@ int main( int argc, char** argv )
 		
 	return 0;
 	
-}
-	else
-{
-printf( "Aborted!" );
-return 0;
-}
-return 0;
+	}
+		else
+	{
+		printf( "Aborted!" );
+		return 0;
+	}
+	return 0;
+	}
+	
 }
